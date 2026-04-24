59a9460c98a102af7c1919b86091e1b21321448d
Mariam <254518771+mariamandrino@users.noreply.github.com>
2026-04-24 10:44:23 +0200

    feat: add and list tasks

--- todo.py
+++ todo.py
@@ -5,15 +5,18 @@ class GestorTareas:
 
     def añadir_tarea(self, nombre):
         self.tareas.append({"nombre": nombre, "completada": False})
-        print(f"Tarea añadida: {nombre}")
+        print(f"Tarea agregada: {nombre}")
 
     def listar_tareas(self):
         if not self.tareas:
             print("No tienes tareas.")
             return
         for i, tarea in enumerate(self.tareas):
-            estado = "✓" if tareas["completada"] else "X"
+            estado = "hecha" if tarea["completada"] else "pendiente"
             print(f"{i + 1}.{estado} {tarea["nombre"]}")
 
 
 gestor = GestorTareas()
+gestor.añadir_tarea("Aprender GIT")
+gestor.añadir_tarea("Terminar todo-cli")
+gestor.listar_tareas()
